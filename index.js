824b0c85ff9b694615605c1778ec1146ce1d5ffc
Chamsseddin <98670814+Chamsseddin@users.noreply.github.com>
2022-05-19 17:23:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 5000;
 const server = http.createServer((req,res) => {
     if (request(req,'/','GET')) {
         res.writeHead(200,{'Content-type':'text/html'});
-        fs.readFile('Backend_road_map/HTML/index.html',(err,content) => {
+        fs.readFile('HTML/index.html',(err,content) => {
             if (err) return res.end(`<h1>Error</h1>`);
             console.log(req.method,req.url);
             res.end(content);
@@ -12,7 +12,7 @@ const server = http.createServer((req,res) => {
     }
     if (request(req,'/backend.png','GET')) {
         res.writeHead(200,{'Content-type':'image/png'});
-        fs.readFile('Backend_road_map/IMAGES/backend.png',(err,content) => {
+        fs.readFile('IMAGES/backend.png',(err,content) => {
             if (err) return res.end(`<h1>Error</h1>`);
             console.log(req.method,req.url);
             res.end(content);
@@ -20,7 +20,7 @@ const server = http.createServer((req,res) => {
     }
     if (request(req,'/script.js','GET')) {
         res.writeHead(200,{'Content-type':'application/javascript'});
-        fs.readFile('Backend_road_map/JS/script.js',(err,content) => {
+        fs.readFile('JS/script.js',(err,content) => {
             if (err) return res.end(`<h1>Error</h1>`);
             console.log(req.method,req.url);
             res.end(content);
@@ -28,7 +28,7 @@ const server = http.createServer((req,res) => {
     }
     if (request(req,'/style.css','GET')) {
         res.writeHead(200,{'Content-type':'text/css'});
-        fs.readFile('Backend_road_map/CSS/style.css',(err,content) => {
+        fs.readFile('CSS/style.css',(err,content) => {
             if (err) return res.end(`<h1>Error</h1>`);
             console.log(req.method,req.url);
             res.end(content);
